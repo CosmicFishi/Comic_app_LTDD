7f1e1a5ab421601cd0b3afd0ffe1b5706feb12f7
CosmicFishi <lyquocdung09@gmail.com>
2021-03-19 14:51:19 +0700

    added first fragment, second fragment

--- Demo/app/src/main/java/com/example/demo_fragment/MainActivity.java
+++ Demo/app/src/main/java/com/example/demo_fragment/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.demo_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    FragmentManager fm = getSupportFragmentManager();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragment();
+            }
+        });
+
+//        getFragment();
+    }
+
+    private void getFragment() {
+        Fragment fragment = fm.findFragmentById(R.id.fl_fragment);
+        if(fragment instanceof FirstFragment){
+            fragment = new SecondFragment();
+        } else {
+            fragment = new FirstFragment();
+        }
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.fl_fragment, fragment);
+        ft.replace(R.id.fl_fragment, fragment);
+//        ft.addToBackStack("abc");
+        ft.commit();
     }
 }
